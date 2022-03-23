a119d787a0cdd87ae5c19ac98b7bd3510b2367ec
Varky <varkychen@gmail.com>
2022-03-23 20:16:16 +0530

    Removing unrequired imports

--- src/main/java/com/bojug/appcds/city/CityController.java
+++ src/main/java/com/bojug/appcds/city/CityController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 @RequestMapping("/v1/city")
 public class CityController {
